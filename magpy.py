8f96e1c2d6c626f6083a5c733103ee63fe7bc503
Danny Goodacre <dannygoodacre1@gmail.com>
2022-07-22 23:56:01 +0100

    Finalise LOp class for non-interacting systems and implement System class.

--- magpy.py
+++ magpy.py
@@ -51,11 +51,10 @@ def unvec(vec, c=None):
         Matrix.
 
     """
-
     vec = np.array(vec)
 
-    # odd number of elements
     if (len(vec) % 2 != 0):
+        # odd number of elements
         if (len(vec) == 1):
             return vec
         else:
@@ -63,15 +62,15 @@ def unvec(vec, c=None):
                   Cannot form matrix.")
             return None
     elif c is None:
-        # matrix is square
         if (np.sqrt(len(vec)).is_integer()):
+            # matrix is square
             c = int(np.sqrt(len(vec)))
         else:
             print("Error: vector cannot form a square matrix. \
                   Please provide a column length, c.")
             return None
-    # c does not divide length of vec
     elif (not (len(vec) / c).is_integer()):
+        # c does not divide length of vec
         print("Error: value of c is invalid. \
               Cannot split vector evenly into columns of length c")
         return None
@@ -97,7 +96,6 @@ def liouvillian(H):
         Square matrix with dimension n^2.
 
     """
-
     n = H.shape[0]
 
     return -1j * (np.kron(np.eye(n), H) - np.kron(H.T, np.eye(n)))
@@ -122,7 +120,6 @@ def commutator(A, B, kind="normal"):
         Commutator of A and B.
 
     """
-
     if kind == "normal":
         return A@B - B@A
     elif kind == "anti":
@@ -146,19 +143,18 @@ def kron(*args):
         No input arguments.
 
     """
-
     if not args:
         raise TypeError("Requires at least one input argument")
 
-    # input of the form [a,b,...]
     if len(args) == 1 and isinstance(args[0], list):
+        # input of the form [a,b,...]
         mlist = args[0]
     elif len(args) == 1 and isinstance(args[0], np.ndarray):
         # single
         if len(args[0].shape) == 2:
             return args[0]
-        # ndarray
         else:
+            # ndarray
             mlist = args[0]
     else:
         mlist = args
@@ -183,12 +179,12 @@ def linspace(start, stop, step, dtype=None):
     step : array_like
         Amount by which to space points in sequence.
     dtype : dtype, optional
-        The type of the output array. If dtype is not given,
-        then the data type is inferred from arguments. The default is None.
+        The type of the output array. If dtype is not given,then the data type 
+        is inferred from arguments. The default is None.
 
     Returns
     -------
-    np.ndarray
+    ndarray
         Equally spaced numbers as specified.
 
     """
@@ -223,146 +219,278 @@ def frobenius(a, b):
     array([2.+0.j, 4.+0.j])
 
     """
-
     a = np.asarray(a, dtype=object)
     b = np.asarray(b, dtype=complex)
 
-    # a is an array
     try:
+        # a is an array
         t = []
         for x in a:
             t.append(np.trace(x.conj().T @ b))
         return np.asarray(t)
 
-    # a is single
     except:
+        # a is single
         return np.trace(a.conj().T @ b)
 
 
-def _magnus_first_term(H_coeffs, HJ, t0, tf):
-    omega1 = (tf - t0) * HJ
-    for j in range(len(H_coeffs)):
-        Ijx = [np.eye(2) for _ in H_coeffs]
-        Ijy = [np.eye(2) for _ in H_coeffs]
-        Ijz = [np.eye(2) for _ in H_coeffs]
-        Ijx[j] = sigmax()
-        Ijy[j] = sigmay()
-        Ijz[j] = sigmaz()
-
-        omega1 = (omega1
-                  + scipy.integrate.quad(H_coeffs[j][0], t0, tf)[0]*kron(Ijx)
-                  + scipy.integrate.quad(H_coeffs[j][1], t0, tf)[0]*kron(Ijy)
-                  + H_coeffs[j][2]*(tf - t0)*kron(Ijz))
-
-    return liouvillian(omega1)
-
-
-def _magnus_second_term(H_coeffs, HJ, t0, tf):
-    omega2 = 0
-    for j in range(len(H_coeffs)):
-        Ijx = [np.eye(2) for _ in H_coeffs]
-        Ijy = [np.eye(2) for _ in H_coeffs]
-        Ijz = [np.eye(2) for _ in H_coeffs]
-        Ijx[j] = sigmax()
-        Ijy[j] = sigmay()
-        Ijz[j] = sigmaz()
-
-        c1 = 2j*H_coeffs[j][2]*kron(Ijx) + commutator(kron(Ijy), HJ)
-        c2 = 2j*H_coeffs[j][2]*kron(Ijy) + commutator(HJ, kron(Ijx))
-        c3 = 2j * kron(Ijz)
-
-        f = H_coeffs[j][0]
-        g = H_coeffs[j][1]
-
-        def x(x): return x
-        def q1(y, x): return g(y) - g(x)
-        def q2(y, x): return f(y) - f(x)
-        def q3(y, x): return f(y)*g(x) - g(y)*f(x)
-
-        int1 = scipy.integrate.dblquad(q1, t0, tf, t0, x)[0]
-        int2 = scipy.integrate.dblquad(q2, t0, tf, t0, x)[0]
-        int3 = scipy.integrate.dblquad(q3, t0, tf, t0, x)[0]
-
-        omega2 = omega2 + int1*c1 - int2*c2 + int3*c3
-
-    return 0.5j * liouvillian(omega2)
-
-
-def lvnsolve(H_coeffs, rho0, tlist, HJ=None):
+class LOp:
     """
-    Liouville-von Neumann evolution of density matrix for given Hamiltonian.
+    Represents a constant quantum operator. This can be for a system containing
+    one spin, for a single spin in part of a larger system, or for an 
+    interactive component of a larger system.
 
-    For n particles, the Hamiltonian takes the form:
-
-    sum_{k=1}^{n} Id otimes  ... otimes (f_k(t)*sigmax + g_k(t)*sigmay
-    + omega_k*sigmaz) otimes  ... otimes Id,
-
-    where k denotes position in the kronecker product (otimes).
-
-    For one particle the Hamiltonian takes the form:
-
-    f(t)*sigmax + g(t)*sigmay + omega*sigmaz.
-
-    H_coeffs then takes the form [[f1, g1, omega1], [f2, g2, omega2], ...],
-    or [f, g, omega] for a single particle.
-
-    f and g must be functions and the omegas are scalar constants.
-
-    Parameters
+    Attributes
     ----------
-    H_coeffs : list / array
-        Coefficients that form Hamiltonian.
-    rho0 : ndarray
-        Initial density matrix.
-    tlist : list / array
-        Times at which to calculate density matrices.
-    HJ : ndarray, optional
-        Interacting part of Hamiltonian, by default None.
-
-    Returns
-    -------
-    numpy.ndarray
-        Density matrices calculated across tlist.
+    data : ndarray
+        Matrix representing the quantum operator.
 
     Examples
     --------
-    One particle :
+    One spin:
+        H = sigmax
 
-        >>> def f(t): return t
-        >>> def g(t): return t - t**2
-        >>> omega = 2
-        >>> H_coeffs = [f, g, omega]
+        >>> mp.LOp(1, 1, mp.sigmax())
 
-    Two particles:
+    Two spin:
+        H = sigmax x Id
 
-        >>> def f1(t): return t
-        >>> def g1(t): return t**2
-        >>> omega1 = 2
-        >>> def f2(t): return 4*t
-        >>> def g2(t): return np.sqrt(t)
-        >>> omega2 = -1
-        >>> H_coeffs = [[f1, g1, omega1], [f2, g2, omega2]]
+        >>> mp.LOp(2, 1, mp.sigmax())
+
+    Two spins interacting:
+        TODO
 
     """
 
-    # check whether H_coeffs is a single particle
-    # if so convert to list containing only that particle's data
-    if not isinstance(H_coeffs[0], (list, np.ndarray)):
-        H_coeffs = [H_coeffs]
+    def __init__(self, n, pos, ham):
+        """
+        Construct matrix representing the quantum operator.
 
-    # check whether HJ is empty and if it is needed to be
-    # compatible with dimension of Hamiltonian
-    if HJ is None:
-        n = len(H_coeffs)
-        HJ = np.zeros((2**n, 2**n))
+        Parameters
+        ----------
+        n : int
+            Number of spins in system. Must be greater than or equal to 1.
+        pos : int
+            Position of given matrix in system. 
+        ham : ndarray
+            Matrix corresponding to specified spin.
 
-    states = [vec(rho0)]
-    for i in range(len(tlist) - 1):
-        omega = (_magnus_first_term(H_coeffs, HJ, tlist[i], tlist[i+1])
-                 + _magnus_second_term(H_coeffs, HJ, tlist[i], tlist[i+1]))
-        states.append(scipy.linalg.expm(omega) @ states[i])
-        states[i] = unvec(states[i])
+        """
+        array = [np.eye(2) for _ in range(n)]
+        array[pos - 1] = ham
 
-    states[-1] = unvec(states[-1])
+        self.data = kron(array)
 
-    return states
+    def __call__(self):
+        return self.data
+
+    def is_hermitian(self):
+        return np.array_equal(self.data.conj().T, self.data)
+
+
+class System:
+    """
+    Represents a quantum system with a specified Hamiltonian, allowing for the
+    simulation of the system using a two-term Magnus expansion without 
+    requiring re-calculation of the Liouvillians each time.
+
+    Parameters
+    ----------
+    H : dict
+        The Hamiltonian of the system, which takes the form:
+
+            H = { f_1 : H_1, f_2 : H_2, ... },
+        
+        where each f_i is a function and each H_i is a LOp object. 
+
+    Attributes
+    ----------
+    H1 : dict
+        Pre-calculated Liouvillians and corresponding functional coefficients
+        for the first term.
+
+        Takes the form: 
+            
+            { f_1 : L(H_1), f_2 : L(H_2), ... },
+
+        where f_i are functions (or constants) and L(H_i) are liouvillians of 
+        the corresponding matrices. L(H_i) are square ndarrays.
+
+    H2 : dict
+        Pre-calculated Liouvillians and corresponding functional coefficients
+        for the second term.
+
+        Takes the form: 
+        
+            { f_1(y)*f_2(x) : L([H_1, H_2]), 
+              f_1(y)*f_3(x) : L([H_1, H_3]), ... },
+                
+        where each entry is the two-variable function and its corresponding
+        liouvillian of a commutator. L([H_i, H_j]) are square ndarrays.
+
+    Examples
+    --------
+    Single spin system: 
+        H(t) = f(t)*sigmax + g(t)*sigmay
+
+        >>> H = {f : mp.LOp(1,1,mp.sigmax()), g : mp.LOp(1,1,mp.sigmay())}
+
+    Two spin system: 
+        H(t) = f(t)*(sigmax x Id) + g(t)*(Id x sigmay)
+
+        >>> H = {f : mp.LOp(2,1,mp.sigmax()), g : mp.LOp(2,2,mp.sigmay())}
+
+    Two spin system with repeated coefficient:
+        H(t) = f(t)*(sigmax x Id) + f(t)*(Id x sigmay) + g(t)*(sigmaz x Id)
+
+        >>> H = {f : [mp.LOp(2,1,mp.sigmax()), mp.LOp(2,2,mp.sigmay())], 
+        g : mp.LOp(2,1,mp.sigmaz())}
+
+    Interacting system:
+        TODO
+
+    """
+
+    def __init__(self, H):
+        """
+        Perform pre-calculations for a two term Magnus expansion, and stored 
+        in H1 and H2 to reduce reptitive calculations.
+
+        Parameters
+        ----------
+        H : dict
+            Hamiltonian of the system, which takes the form:
+
+                H = { f_1 : H_1, f_2 : H_2, ... },
+            
+            where f_i are functions and H_i are LOp objects.
+
+        """
+        self.H1 = self.__setup_first_term(H)
+        self.H2 = self.__setup_second_term(H)
+
+    def update_hamiltonian(self, H):
+        self.__init__(H)
+
+    def __setup_first_term(self, H):
+        """
+        Pre-calculate Liouvillians for matrices (LOp) in H 
+        for first term.
+        """
+        H1 = {}
+
+        for coeff in H:
+            if isinstance(H[coeff], LOp):
+                # convert single LOp to list of (single) LOp
+                H[coeff] = [H[coeff]]
+
+            matrices = []
+            for matrix in H[coeff]:
+                matrices.append(liouvillian(matrix()))
+            
+            H1[coeff] = matrices
+        
+        return H1
+
+    def __setup_second_term(self, H):
+        """
+        Pre-calculate Liouvillians for matrices (LOp) in H 
+        for first term.
+        """
+        temp = []
+
+        for coeff in H:
+            if isinstance(H[coeff], list):
+                for matrix in H[coeff]:
+                    temp.append((coeff, matrix()))
+            else:
+                temp.append((coeff, H[coeff]()))
+
+        n = len(temp)
+        H2 = {}
+
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    if (isinstance(temp[i][0], (int, float)) 
+                        and isinstance(temp[j][0], (int, float))):
+                        f = temp[i][0] * temp[j][0]
+                    elif isinstance(temp[i][0], (int, float)):
+                        def f(y, x): return temp[i][0] * temp[j][0](x)
+                    elif isinstance(temp[j][0], (int, float)):
+                        def f(y, x): return temp[i][0](y) * temp[j][0]
+                    else:
+                        def f(y,x): return temp[i][0](y) * temp[j][0](x)
+
+                    H2[f] = 1j * liouvillian(commutator(temp[i][1](), 
+                                                                temp[j][1]()))
+        
+        return H2
+
+    def __eval_first_term(self, t0, tf):
+        """
+        Evaluate first term of Magnus expansion over specified time
+        interval using pre-calculated terms.
+        """
+        total = 0
+
+        for coeff in self.H1:
+            if isinstance(coeff, (int, float)):
+                for matrix in self.H1[coeff]:
+                    total = total + matrix*coeff*(tf - t0)
+            else:
+                c = scipy.integrate.quad(coeff, t0, tf)[0]
+                for matrix in self.H1[coeff]:
+                    total = total + c*matrix
+        
+        return total
+
+    def __eval_second_term(self, t0, tf):
+        """
+        Evaluate second term of Magnus expansion over specified time
+        interval using pre-calculated terms.
+        """
+        total = 0
+        def x(x): return x
+
+        for coeff in self.H2:
+            c = scipy.integrate.dblquad(coeff, t0, tf, t0, x)[0]
+            total = total + c*self.H2[coeff]
+
+        return total
+
+    def lvn_solve(self, rho0, tlist):
+        """
+        Liouville-von Neumann evolution of density matrix for given 
+        Hamiltonian.
+
+        Parameters
+        ----------
+        rho0 : ndarray
+            Initial density matrix.
+        tlist : list / array
+            Times at which to evaluate density matrices.
+
+        Returns
+        -------
+        ndarray
+            Density matrices calculated across tlist.
+
+        Examples
+        --------
+        TODO
+
+        """
+        states = [vec(rho0())]
+
+        for i in range(len(tlist) - 1):
+
+            omega = (self.__eval_first_term(tlist[i], tlist[i+1])
+                     + self.__eval_second_term(tlist[i], tlist[i+1]))
+
+            states.append(scipy.linalg.expm(omega) @ states[i])
+            states[i] = unvec(states[i])
+        
+        states[-1] = unvec(states[-1])
+
+        return states
