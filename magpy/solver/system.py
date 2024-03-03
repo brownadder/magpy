846a0e691ada4af12434c10419ca49f1f910973f
dannygodacre <danny.goodacre@bath.edu>
2024-03-03 22:31:27 +0000

    Add draft 1-term Magnus

--- magpy/solver/system.py
+++ magpy/solver/system.py
@@ -1,4 +1,5 @@
 import torch
+import scipy.integrate
 from abc import ABC, abstractmethod
 
 
@@ -26,6 +27,7 @@ class System(ABC):
 
         if H.is_constant():
             return _TimeIndependentQuantumSystem(H, rho0, tlist)
+        return _TimeDependentQuantumSystem(H, rho0, tlist)
 
     @abstractmethod
     def evolve(self):
@@ -48,3 +50,31 @@ class _TimeIndependentQuantumSystem(System):
 
         for i in range(len(self.tlist) - 1):
             self.states.append(lhs @ self.states[i] @ rhs)
+
+
+class _TimeDependentQuantumSystem(System):
+
+    def __init__(self, H, rho0, tlist):
+        self.H = H
+        self.rho0 = rho0
+        self.tlist = tlist
+        self.states = None
+
+    def evolve(self):
+        self.states = [self.rho0()]
+
+        # Draft version of 1-term Magnus.
+        temp = []
+        for f, h in self.H.data.items():
+            if isinstance(h, list):
+                for e in h:
+                    temp.append((f, e))
+            else:
+                temp.append((f, h))
+
+        for i in range(len(self.tlist) - 1):
+            omega = -1j*sum(scipy.integrate.quad(f, self.tlist[i], self.tlist[i+1])[0] * h() for f, h in temp)
+            u = torch.matrix_exp(omega)
+            ut = torch.conj(torch.transpose(u, 0, 1))
+
+            self.states.append(u @ self.states[i] @ ut)
