e036e840c49f7ddc60245eb868e1525f1b5b1901
Danny Goodacre <dannygoodacre1@gmail.com>
2024-05-20 21:42:30 +0100

    Update root init

--- magpy/__init__.py
+++ magpy/__init__.py
@@ -1,2 +1,2 @@
-from .core import PauliString, FunctionProduct, HamiltonianOperator, kron, frobenius, commutator
-from .solver import System
+from .core import PauliString, FunctionProduct, HamiltonianOperator, kron, frobenius, commutator, timegrid
+from .solver import System, batch_first_term, batch_second_term
